b2c030628a99013642384e22bcb523347e2bd72b
Ilya <i1ya1l@yandex.ru>
2019-03-20 10:53:22 +0300

    brain-even, вынес некоторые функции в index и добавил вывод имени в console вместо Sam

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -1,35 +1,35 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
-import { welcome, askName } from '..';
-
-welcome();
-console.log('Answer "yes" if number even otherwise answer "no".');
-
-askName('May I have your name? ');
-
-const genarateNumber = () => Math.floor(Math.random() * 100);
+import { welcome, getName, genarateNumber} from '..';
 
 let count = 0;
 const correctAnswer = 3;
 
+welcome();
+console.log('Answer "yes" if number even otherwise answer "no".');
+const userName = getName('May I have your name? ');
+console.log(`Hello ${userName}`);
+
 const comparison = number => ((number % 2 === 0) ? 'yes' : 'no');
 
 const start = () => {
+
   const getNumber = genarateNumber();
   console.log(`Question: ${getNumber}`);
+
   const answer = readlineSync.question('Your answer: ');
 
   if (answer === comparison(getNumber)) {
     count += 1;
     console.log('Correct!');
     if (count === correctAnswer) {
-      console.log('Congratulations, Bill!');
+      console.log(`Congratulations, ${userName}!`);
       return;
     }
     start();
   } else {
     console.log("'yes' is wrong answer ;(. Correct answer was 'no'.");
-    console.log("Let's try again, Bill!");
+    console.log(`Let's try again, ${userName}!`);
   }
 };
 start();
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import readlineSync from 'readline-sync';
+
 export const welcome = () => {
   console.log('Welcome to the Brain Games!');
 };
-export const askName = (question) => {
-  const name = readlineSync.question(question);
-  console.log(`Hello ${name}`);
-};
+export const getName = (question) => {
+	const name = readlineSync.question(question);
+	return name;
+}
+export const genarateNumber = () => Math.floor(Math.random() * 100);
