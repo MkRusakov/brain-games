224cc941559fd06408001211496d9b6273333c72
Ilya <i1ya1l@yandex.ru>
2019-03-20 21:35:19 +0300

    brains-even правки

--- src/games/even.js
+++ src/games/even.js
@@ -1,37 +1,26 @@
-#!/usr/bin/env node
-import readlineSync from 'readline-sync';
-import { welcome, getName, genarateNumber } from '..';
+import { gamePlay, genarateNumber } from '..';
+
+const isEven = number => ((number % 2 === 0));
+
+const model = {
+  description : 'Answer "yes" if number even otherwise answer "no".',
+  number : null,
+  question () {
+    const getNumber = genarateNumber();
+    console.log(`Question: ${getNumber}`);
+    this.number = getNumber;
+  },
+  getAnswer() {
+    if (isEven(this.number)) {
+      return 'yes';
+    } {
+      return 'no';
+    }
+  }
+  
+}
 
 const even = () => {
-  welcome();
-  console.log('Answer "yes" if number even otherwise answer "no".');
-  const userName = getName('May I have your name? ');
-  console.log(`Hello ${userName}`);
-
-  const isEven = number => ((number % 2 === 0));
-  const userAnswer = answer => ((answer === 'yes'));
-  const start = () => {
-    let countSteps = 0;
-    const limitSteps = 3;
-    for (countSteps; countSteps <= limitSteps;) {
-      const getNumber = genarateNumber();
-      console.log(`Question: ${getNumber}`);
-      const answer = readlineSync.question('Your answer: ');
-      if (userAnswer(answer) === isEven(getNumber)) {
-        countSteps += 1;
-        console.log('Correct!');
-        if (countSteps === limitSteps) {
-          console.log(`Congratulations, ${userName}!`);
-          return;
-        }
-      } else {
-        console.log("'yes' is wrong answer ;(. Correct answer was 'no'.");
-        console.log(`Let's try again, ${userName}!`);
-        return;
-      }
-    }
-  };
-
-  start();
+  gamePlay(model);
 };
 export default even;
--- src/index.js
+++ src/index.js
@@ -8,3 +8,35 @@ export const getName = (question) => {
   return name;
 };
 export const genarateNumber = (number = 100) => Math.floor(Math.random() * number);
+
+
+export const gamePlay = (gameSetting) => {
+	console.log('Welcome to the Brain Games!');
+	console.log(gameSetting.description);
+
+	const userName = getName('May I have your name? ');
+	console.log(`Hello ${userName}`);
+	const startGame = () => {
+
+		const limitSteps = 3;
+		for (let countSteps = 0; countSteps <= limitSteps;) {
+			gameSetting.question();
+		  
+		  const userAnswer = readlineSync.question('Your answer: ');
+		  if (userAnswer === gameSetting.getAnswer()) {
+		    countSteps += 1;
+		    console.log('Correct!');
+		    if (countSteps === limitSteps) {
+		      console.log(`Congratulations, ${userName}!`);
+		      return;
+		    }
+		  } else {
+		    console.log("'yes' is wrong answer ;(. Correct answer was 'no'.");
+		    console.log(`Let's try again, ${userName}!`);
+		    return;
+		  }
+		}
+	};
+
+	startGame();
+}
