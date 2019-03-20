257e945bb390088acdefb08e1bb55d55c7c18ccc
Ilya <i1ya1l@yandex.ru>
2019-03-20 21:40:20 +0300

    linter

--- src/games/even.js
+++ src/games/even.js
@@ -3,22 +3,19 @@ import { gamePlay, genarateNumber } from '..';
 const isEven = number => ((number % 2 === 0));
 
 const model = {
-  description : 'Answer "yes" if number even otherwise answer "no".',
-  number : null,
-  question () {
+  description: 'Answer "yes" if number even otherwise answer "no".',
+  number: null,
+  question() {
     const getNumber = genarateNumber();
     console.log(`Question: ${getNumber}`);
     this.number = getNumber;
   },
   getAnswer() {
-    if (isEven(this.number)) {
-      return 'yes';
-    } {
-      return 'no';
-    }
-  }
-  
-}
+    // eslint-disable-next-line no-unused-expressions
+    (isEven(this.number)) ? 'yes' : 'no';
+  },
+
+};
 
 const even = () => {
   gamePlay(model);
--- src/index.js
+++ src/index.js
@@ -11,32 +11,31 @@ export const genarateNumber = (number = 100) => Math.floor(Math.random() * numbe
 
 
 export const gamePlay = (gameSetting) => {
-	console.log('Welcome to the Brain Games!');
-	console.log(gameSetting.description);
+  console.log('Welcome to the Brain Games!');
+  console.log(gameSetting.description);
 
-	const userName = getName('May I have your name? ');
-	console.log(`Hello ${userName}`);
-	const startGame = () => {
+  const userName = getName('May I have your name? ');
+  console.log(`Hello ${userName}`);
+  const startGame = () => {
+    const limitSteps = 3;
+    for (let countSteps = 0; countSteps <= limitSteps;) {
+      gameSetting.question();
 
-		const limitSteps = 3;
-		for (let countSteps = 0; countSteps <= limitSteps;) {
-			gameSetting.question();
-		  
-		  const userAnswer = readlineSync.question('Your answer: ');
-		  if (userAnswer === gameSetting.getAnswer()) {
-		    countSteps += 1;
-		    console.log('Correct!');
-		    if (countSteps === limitSteps) {
-		      console.log(`Congratulations, ${userName}!`);
-		      return;
-		    }
-		  } else {
-		    console.log("'yes' is wrong answer ;(. Correct answer was 'no'.");
-		    console.log(`Let's try again, ${userName}!`);
-		    return;
-		  }
-		}
-	};
+      const userAnswer = readlineSync.question('Your answer: ');
+      if (userAnswer === gameSetting.getAnswer()) {
+        countSteps += 1;
+        console.log('Correct!');
+        if (countSteps === limitSteps) {
+          console.log(`Congratulations, ${userName}!`);
+          return;
+        }
+      } else {
+        console.log("'yes' is wrong answer ;(. Correct answer was 'no'.");
+        console.log(`Let's try again, ${userName}!`);
+        return;
+      }
+    }
+  };
 
-	startGame();
-}
+  startGame();
+};
